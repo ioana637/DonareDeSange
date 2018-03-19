e84e2e2ad386a8ae4cb6ae88a7099b02686667b5
ioana637 <ioana_claudia_37@yahoo.com>
2018-03-19 20:02:45 +0200

    Proiect

---
+++ CentruDeTransfuzie1/Data/CTContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentruDeTransfuzie1
+{
+    public class CTContext:DbContext
+    {
+        public CTContext(DbContextOptions<CTContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+        }
+
+    }
+
+   
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using CentruDeTransfuzie1;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db=new CTContext(new DbContextOptions<CTContext>()))
+            {
+                db.Database.EnsureCreated();
+                Console.Write("Hello World");
+            }
+        }
+    }
+}
