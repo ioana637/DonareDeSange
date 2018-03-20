ad7c0f3f792d7e2dda74be30deb5275b0765c140
teodorabrsvn <teodorabrsvn@yahoo.com>
2018-03-19 23:22:49 -0800

    GUI Medic

---
+++ GUI/FormLogareMedic.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GUI
+{
+    partial class FormLogareMedic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.Location = new System.Drawing.Point(142, 208);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogIn.TabIndex = 0;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = true;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBoxUsername.Location = new System.Drawing.Point(98, 76);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(169, 20);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.Tag = "";
+            this.textBoxUsername.Text = "username";
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.textBoxPassword.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxPassword.Location = new System.Drawing.Point(98, 143);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(169, 20);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.Text = "password";
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // FormLogareMedic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 318);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.buttonLogIn);
+            this.Name = "FormLogareMedic";
+            this.Text = "LOG IN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
