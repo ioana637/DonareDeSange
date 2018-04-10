5463df3f1bcd10392b11e1b124d19c18243b7f3d
geaninacosovanu <cosovanugeanina@gmail.com>
2018-04-10 11:56:23 +0300

    Criptare parola
    Am criptat parola pentru InregistrareDonator, iar la logare se cripteaza parola introdusa in textBox si se compara cu cea din baza de date. Am adaugat un Donator si un userDonator cu username=popescu si parola=parola.

--- CentruDeTransfuzie/Data/DbInitializer.cs
+++ CentruDeTransfuzie/Data/DbInitializer.cs
@@ -15,8 +15,13 @@ namespace CentruDeTransfuzie1.Data
 
             Medic m1 = new Medic("2770111123456", "Coste", "Claudia", "ioana@yahoo.com", "0742345678");
             Medic m2 = new Medic("2661223654321", "Dordai", "Raul", "raul@yahoo.com", "0785123456");
+            Donator donator1 = new Donator("Popescu", "Ion", "M", DateTime.Parse("12-02-98"), "Cluj-Napoca", "Cluj-Napoca", "Cluj", null, null, null, "0712121212", "popescu@yahoo.com");
+            UserDonator userDonator1 = new UserDonator("popescu", "8287458823facb8ff918dbfabcd22ccb"); // parola este "parola"
             UserMedic um1 = new UserMedic("ioana", "12345");
             UserMedic um2 = new UserMedic("raul", "12345");
+            context.Donator.Add(donator1);
+            context.UserDonator.Add(userDonator1);
+            context.SaveChanges();
             var usersM = new UserMedic[]
             {
                 um1, um2
