36a61212bf87b9f75526b6a73cc032ba870f6c24
anymarydo <ana_dobry23@yahoo.com>
2018-04-11 14:28:37 +0300

    fctii intialize

--- CentruDeTransfuzie/Data/DbInitializer.cs
+++ CentruDeTransfuzie/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,48 +8,38 @@ namespace CentruDeTransfuzie1.Data
 {
     public static class DbInitializer
     {
+
+
         public static void Initialize(CTContext context)
         {
-            context.Database.EnsureCreated();
-            if (context.Cerere.Any()) return;
-            Cerere c1 = new Cerere("1-1-2018", 5, 1, 3, 1, true, context.Medic.Find(1));
-            Cerere c2 = new Cerere("2-1-2018", 7, 2, 1, 4, false, context.Medic.Find(2));
-            context.Cerere.Add(c1);
-            context.Cerere.Add(c2);
-            context.SaveChanges();
 
-            if (context.UserMedic.Any()) return;
+            context.Database.Migrate();
+            initializeMedic(context);
+            InitializeCereri(context);
+            InitializeDonatori(context);
 
+
+        }
+
+
+        public static void initializeMedic(CTContext context)
+        {
             Medic m1 = new Medic("2770111123456", "Coste", "Claudia", "ioana@yahoo.com", "0742345678");
             Medic m2 = new Medic("2661223654321", "Dordai", "Raul", "raul@yahoo.com", "0785123456");
-            Donator donator1 = new Donator("Popescu", "Ion", "M", DateTime.Parse("12-02-98"), "Cluj-Napoca", "Cluj-Napoca", "Cluj", null, null, null, "0712121212", "popescu@yahoo.com");
-            UserDonator userDonator1 = new UserDonator("popescu", "8287458823facb8ff918dbfabcd22ccb"); // parola este "parola"
+            var medici = new Medic[] { m1, m2 };
+            foreach (Medic m in medici)
+            {
+                context.Medic.Add(m);
+            }
             UserMedic um1 = new UserMedic("ioana", "12345");
             UserMedic um2 = new UserMedic("raul", "12345");
-            context.Donator.Add(donator1);
-            context.UserDonator.Add(userDonator1);
-            
-            context.SaveChanges();
-            var usersM = new UserMedic[]
-            {
-                um1, um2
-            };
-            var medici = new Medic[] { m1, m2 };
-            var cereri = new Cerere[] { c1, c2 };
-
+            var usersM = new UserMedic[]{ um1, um2 };
             foreach (UserMedic um in usersM)
             {
 
                 context.UserMedic.Add(um);
             }
-            foreach (Medic m in medici)
-            {
-                context.Medic.Add(m);
-            }
-            foreach(Cerere c in cereri)
-            {
-                context.Cerere.Add(c);
-            }
+
             context.SaveChanges();
             context.UserMedic.Find(1).Medic = context.Medic.Find(1);
             context.Medic.Find(1).UserMedic = context.UserMedic.Find(1);
@@ -56,6 +47,38 @@ namespace CentruDeTransfuzie1.Data
             context.Medic.Find(2).UserMedic = context.UserMedic.Find(2);
 
             context.SaveChanges();
+
         }
+
+        static void InitializeCereri(CTContext context)
+        {
+            if (context.Cerere.Any()) return;
+            Cerere c1 = new Cerere("1-1-2018", 5, 1, 3, 1, true, context.Medic.Find(1));
+            Cerere c2 = new Cerere("2-1-2018", 7, 2, 1, 4, false, context.Medic.Find(2));
+            var cereri = new Cerere[] { c1, c2 };
+            foreach (Cerere c in cereri)
+            {
+                context.Cerere.Add(c);
+            }
+            context.SaveChanges();
+        }
+
+        static void InitializeDonatori(CTContext context)
+        {
+            if (context.Donator.Any()) return;
+
+            Donator donator1 = new Donator("Popescu", "Ion", "M", DateTime.Parse("12-02-98"), "Cluj-Napoca", "Cluj-Napoca", "Cluj", null, null, null, "0712121212", "popescu@yahoo.com");
+            UserDonator userDonator1 = new UserDonator("popescu", "8287458823facb8ff918dbfabcd22ccb"); // parola este "parola"
+
+            context.Donator.Add(donator1);
+            context.UserDonator.Add(userDonator1);
+
+            context.SaveChanges();
+
+        }
+
+
+
+
     }
 }
