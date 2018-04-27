e94437aa98806f891b791116f2da22c9eb2c4616
geaninacosovanu <cosovanugeanina@gmail.com>
2018-04-27 22:08:56 +0300

    CreareForm

---
+++ GUI/FormTrimitereAnalize.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace GUI
+{
+    partial class FormTrimitereAnalize
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxRh = new System.Windows.Forms.ComboBox();
+            this.comboBoxGrupaSange = new System.Windows.Forms.ComboBox();
+            this.checkBoxHIV = new System.Windows.Forms.CheckBox();
+            this.checkBoxHepatitaB = new System.Windows.Forms.CheckBox();
+            this.checkBoxHepatitaC = new System.Windows.Forms.CheckBox();
+            this.checkBoxSifilis = new System.Windows.Forms.CheckBox();
+            this.checkBoxHTLV = new System.Windows.Forms.CheckBox();
+            this.textBoxNivelALT = new System.Windows.Forms.TextBox();
+            this.comboBoxPunga = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonTrimite = new System.Windows.Forms.Button();
+            this.buttonAnulare = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(106, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rh";
+            // 
+            // comboBoxRh
+            // 
+            this.comboBoxRh.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBoxRh.FormattingEnabled = true;
+            this.comboBoxRh.Location = new System.Drawing.Point(195, 32);
+            this.comboBoxRh.Name = "comboBoxRh";
+            this.comboBoxRh.Size = new System.Drawing.Size(172, 28);
+            this.comboBoxRh.TabIndex = 1;
+            // 
+            // comboBoxGrupaSange
+            // 
+            this.comboBoxGrupaSange.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBoxGrupaSange.FormattingEnabled = true;
+            this.comboBoxGrupaSange.Location = new System.Drawing.Point(195, 66);
+            this.comboBoxGrupaSange.Name = "comboBoxGrupaSange";
+            this.comboBoxGrupaSange.Size = new System.Drawing.Size(172, 28);
+            this.comboBoxGrupaSange.TabIndex = 2;
+            // 
+            // checkBoxHIV
+            // 
+            this.checkBoxHIV.AutoSize = true;
+            this.checkBoxHIV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBoxHIV.Location = new System.Drawing.Point(193, 113);
+            this.checkBoxHIV.Name = "checkBoxHIV";
+            this.checkBoxHIV.Size = new System.Drawing.Size(56, 24);
+            this.checkBoxHIV.TabIndex = 4;
+            this.checkBoxHIV.Text = "HIV";
+            this.checkBoxHIV.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxHepatitaB
+            // 
+            this.checkBoxHepatitaB.AutoSize = true;
+            this.checkBoxHepatitaB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBoxHepatitaB.Location = new System.Drawing.Point(263, 113);
+            this.checkBoxHepatitaB.Name = "checkBoxHepatitaB";
+            this.checkBoxHepatitaB.Size = new System.Drawing.Size(104, 24);
+            this.checkBoxHepatitaB.TabIndex = 5;
+            this.checkBoxHepatitaB.Text = "Hepatita B";
+            this.checkBoxHepatitaB.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxHepatitaC
+            // 
+            this.checkBoxHepatitaC.AutoSize = true;
+            this.checkBoxHepatitaC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBoxHepatitaC.Location = new System.Drawing.Point(263, 153);
+            this.checkBoxHepatitaC.Name = "checkBoxHepatitaC";
+            this.checkBoxHepatitaC.Size = new System.Drawing.Size(104, 24);
+            this.checkBoxHepatitaC.TabIndex = 6;
+            this.checkBoxHepatitaC.Text = "Hepatita C";
+            this.checkBoxHepatitaC.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxSifilis
+            // 
+            this.checkBoxSifilis.AutoSize = true;
+            this.checkBoxSifilis.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBoxSifilis.Location = new System.Drawing.Point(193, 196);
+            this.checkBoxSifilis.Name = "checkBoxSifilis";
+            this.checkBoxSifilis.Size = new System.Drawing.Size(64, 24);
+            this.checkBoxSifilis.TabIndex = 7;
+            this.checkBoxSifilis.Text = "Sifilis";
+            this.checkBoxSifilis.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxHTLV
+            // 
+            this.checkBoxHTLV.AutoSize = true;
+            this.checkBoxHTLV.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.checkBoxHTLV.Location = new System.Drawing.Point(193, 153);
+            this.checkBoxHTLV.Name = "checkBoxHTLV";
+            this.checkBoxHTLV.Size = new System.Drawing.Size(69, 24);
+            this.checkBoxHTLV.TabIndex = 8;
+            this.checkBoxHTLV.Text = "HTLV";
+            this.checkBoxHTLV.UseVisualStyleBackColor = true;
+            // 
+            // textBoxNivelALT
+            // 
+            this.textBoxNivelALT.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBoxNivelALT.Location = new System.Drawing.Point(195, 236);
+            this.textBoxNivelALT.Name = "textBoxNivelALT";
+            this.textBoxNivelALT.Size = new System.Drawing.Size(172, 26);
+            this.textBoxNivelALT.TabIndex = 9;
+            this.textBoxNivelALT.Tag = "";
+            // 
+            // comboBoxPunga
+            // 
+            this.comboBoxPunga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.comboBoxPunga.FormattingEnabled = true;
+            this.comboBoxPunga.Location = new System.Drawing.Point(195, 285);
+            this.comboBoxPunga.Name = "comboBoxPunga";
+            this.comboBoxPunga.Size = new System.Drawing.Size(172, 28);
+            this.comboBoxPunga.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Grupă Sânge";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(82, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Nivel ALT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(71, 288);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Pungă Sânge";
+            // 
+            // buttonTrimite
+            // 
+            this.buttonTrimite.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonTrimite.Location = new System.Drawing.Point(90, 363);
+            this.buttonTrimite.Name = "buttonTrimite";
+            this.buttonTrimite.Size = new System.Drawing.Size(121, 33);
+            this.buttonTrimite.TabIndex = 14;
+            this.buttonTrimite.Text = "Trimite";
+            this.buttonTrimite.UseVisualStyleBackColor = true;
+            this.buttonTrimite.Click += new System.EventHandler(this.buttonTrimite_Click);
+            // 
+            // buttonAnulare
+            // 
+            this.buttonAnulare.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.buttonAnulare.Location = new System.Drawing.Point(238, 363);
+            this.buttonAnulare.Name = "buttonAnulare";
+            this.buttonAnulare.Size = new System.Drawing.Size(129, 33);
+            this.buttonAnulare.TabIndex = 15;
+            this.buttonAnulare.Text = "Anulează";
+            this.buttonAnulare.UseVisualStyleBackColor = true;
+            this.buttonAnulare.Click += new System.EventHandler(this.buttonAnulare_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(86, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Boli";
+            // 
+            // FormTrimitereAnalize
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 458);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonAnulare);
+            this.Controls.Add(this.buttonTrimite);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxPunga);
+            this.Controls.Add(this.textBoxNivelALT);
+            this.Controls.Add(this.checkBoxHTLV);
+            this.Controls.Add(this.checkBoxSifilis);
+            this.Controls.Add(this.checkBoxHepatitaC);
+            this.Controls.Add(this.checkBoxHepatitaB);
+            this.Controls.Add(this.checkBoxHIV);
+            this.Controls.Add(this.comboBoxGrupaSange);
+            this.Controls.Add(this.comboBoxRh);
+            this.Controls.Add(this.label1);
+            this.Name = "FormTrimitereAnalize";
+            this.Text = "FormTrimitereAnalize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxRh;
+        private System.Windows.Forms.ComboBox comboBoxGrupaSange;
+        private System.Windows.Forms.CheckBox checkBoxHIV;
+        private System.Windows.Forms.CheckBox checkBoxHepatitaB;
+        private System.Windows.Forms.CheckBox checkBoxHepatitaC;
+        private System.Windows.Forms.CheckBox checkBoxSifilis;
+        private System.Windows.Forms.CheckBox checkBoxHTLV;
+        private System.Windows.Forms.TextBox textBoxNivelALT;
+        private System.Windows.Forms.ComboBox comboBoxPunga;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonTrimite;
+        private System.Windows.Forms.Button buttonAnulare;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ GUI/FormTrimitereAnalize.cs
@@ -0,0 +1,33 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class FormTrimitereAnalize : Form
+    {
+        private CentruService service;
+        public FormTrimitereAnalize(CentruService service)
+        {
+            this.service = service;
+            InitializeComponent();
+        }
+
+        private void buttonTrimite_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAnulare_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
