60ae7911e5de8ff955cb3c394bdb142d54aec663
Raul Dordai <raul_dordai@yahoo.com>
2018-05-28 17:47:26 +0300

    testAddAnalize

--- UnitTestProject1/UnitTestCentruService.cs
+++ UnitTestProject1/UnitTestCentruService.cs
@@ -6,12 +6,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CentruDeTransfuzie.utils;
 
 namespace UnitTestProject1
 {
     class UnitTestCentruService
     {
         CentruService centruService = new CentruService();
+        private GrupaSange BIII;
+        private TipRh Pozitiv;
 
         [TestMethod]
         public void testGetTraseuPunga()
@@ -48,6 +51,24 @@ namespace UnitTestProject1
 
             Assert.AreNotEqual(service.GetAllPungiSange().Where(x => x.CantitateGlobuleRosii == -1), null);
 
+        }
+
+        [TestMethod]
+
+        public void testAddAnalize()
+        {  // NU RULATI , nu este delete
+            DonatorService srvDon = new DonatorService();
+            Donator donator = srvDon.GetDonator("Dmihai");
+            CentruService service = new CentruService();
+
+            PungaSange punga = service.GetPunga(1);
+
+            service.AddAnaliza(BIII,Pozitiv,false,false,true,false,false,-774,punga,donator);
+
+            Assert.AreEqual(srvDon.GetAllAnalizeByIdDonator("Dmihai").Where(x => x.NivelALT == -774), 1);
+
+            //serivice.DeleteAnaliza(analiza) 
+          
         }
 
     }
